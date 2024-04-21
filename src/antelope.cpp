785c69e861e1804feaf5a560ee9af3428309f63f
Yan <mkaspor12345@gmail.com>
2024-04-21 16:05:33 +0200

    antelope behave override

--- src/antelope.cpp
+++ src/antelope.cpp
@@ -1,5 +1,16 @@
 #include <antelope.h>
+#include <world.h>
+#include <ncurses.h>
 
 antelope::antelope(int power, int initiative, int posX, int posY, world *w) : animal(power, initiative, posX, posY, w)
 {
+}
+void antelope::move()
+{
+    this->w->randomMove(&posX, &posY, 2);
+}
+
+void antelope::behave()
+{
+    move();
 }
--- src/include/antelope.h
+++ src/include/antelope.h
@@ -6,4 +6,6 @@ class antelope : public animal
 {
 public:
     antelope(int power, int initiative, int posX, int posY, world *w);
+    void move();
+    void behave();
 };
